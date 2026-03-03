79fad5d702e9499cf231f92e1d4fb7a94f59b49a
mayaboiaryna <162012901+mayaboiaryna@users.noreply.github.com>
2026-03-03 22:57:18 +0100

    Zadanie 3 - translate & translated

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Point p = new Point();
-        p.x = 50;
-        p.y = 80;
-        System.out.println(p.toSvg());
+        p.x = 10;
+        p.y = 20;
+        System.out.println("start p = " + p);
+
+        p.translate(5, -2);
+        System.out.println("after translate p = " + p);
+
+        Point q = p.translated(100, 100);
+        System.out.println("q = " + q);
+        System.out.println("p = " + p);
         }
     }
--- src/Point.java
+++ src/Point.java
@@ -8,4 +8,14 @@ public class Point {
     public String toSvg(){
         return "<circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"5\" fill=\"black\" />";
     }
+    public void translate(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+    public Point translated(double dx, double dy){
+        Point p = new Point();
+        p.x = this.x + dx;
+        p.y = this.y + dy;
+        return p;
+    }
 }
